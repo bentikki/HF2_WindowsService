e3a700f78a32a81217f12591ef39ad4974b8fdb0
Benjamin Tikki Olsen <benjamin.tikki@gmail.com>
2020-03-04 09:41:42 +0100

    Done_Version_1

---
+++ TestWindowsService/TestWindowsService/Library.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestWindowsService
+{
+    public static class Library
+    {
+
+        public static void WriteErrorLog(Exception ex)
+        {
+            try
+            {
+
+                string message = $"{ ex.Source.ToString().Trim() }; { ex.Message.ToString().Trim()}";
+                WriteErrorLog(message);
+            }
+            catch 
+            {
+
+                throw;
+            }
+        }
+
+        public static void WriteErrorLog(string Message)
+        {
+            StreamWriter sw = null;
+            try
+            {
+                sw = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "\\Logfile.txt", true);
+                sw.WriteLine($"{DateTime.Now.ToString()}: {Message}");
+                sw.Flush();
+                sw.Close();
+            }
+            catch
+            {
+
+                throw;
+            }
+        }
+    }
+}
